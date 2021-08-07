40646f6ca1b40f101e76ce58dbba053154f7b7d1
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-07 13:49:25 -0500

    add pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+    <Router>
+        
+    </Router>
+)
+ReactDOM.render(routing, document.getElementById('root')
 );
 
 
